e1f1b93fa13dbdecd28b12d792bc2f9c59417105
Sébastiien Reuiller <sebastien@reuiller.email>
2021-06-03 13:55:13 +0200

    update json usage

--- HTTP/chuck.py
+++ HTTP/chuck.py
@@ -2,7 +2,15 @@
 # coding: utf8
 
 import requests
+import json
 
 r = requests.get("https://api.chucknorris.io/jokes/random")
-print(r.status_code)
-print(r.text)
+
+if r.status_code == 200:
+
+    data = r.json()
+
+    print(json.dumps(data, sort_keys=True, indent=4))
+
+    print(data.get('id'))
+    print(data.get('value'))
