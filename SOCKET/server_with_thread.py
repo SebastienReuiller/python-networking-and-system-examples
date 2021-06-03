31d26265a83f64ba8cded48050ddfc790b923997
Sébastiien Reuiller <sebastien@reuiller.email>
2021-06-03 17:03:46 +0200

    args separate

--- SOCKET/server_with_thread.py
+++ SOCKET/server_with_thread.py
@@ -16,11 +16,11 @@ def handle_client_cnx(cnx, addr):
             print(f"serveur reçois de {addr} : {data.decode()}")
 
 
-def _main(args):
+def _main(server_bind, server_port):
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.bind((args.bind, args.port))
+        s.bind((server_bind, server_port))
         s.listen()
-        print(f"Le serveur est en écoute sur {args.bind}:{args.port}")
+        print(f"Le serveur est en écoute sur {server_bind}:{server_port}")
         while True:
             print('Attente de connexion')
             conn, addr = s.accept()
@@ -34,7 +34,7 @@ if __name__ == '__main__':
     parser.add_argument('-b', '--bind-address',
                         dest='bind',
                         default='0.0.0.0',
-                        help="Adresse IP d'écoute"
+                        help="Adresse IP d'écoute (écoute sur toutes les interfaces par défaut, 0.0.0.0)"
                         )
     parser.add_argument('-p', '--port',
                         dest='port',
@@ -45,4 +45,4 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
 
-    _main(args)
+    _main(args.bind, args.port)
